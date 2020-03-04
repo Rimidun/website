39c18510c196fead9cdba8f07a91307179aea64a
x137@yandex.ru <1k2b3c4C>
2020-03-04 09:45:59 +0300

    1 part, version 1.3

--- src/main/java/com/example/website/domain/Message.java
+++ src/main/java/com/example/website/domain/Message.java
@@ -5,12 +5,11 @@ import javax.persistence.*;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+
     private String text;
     private String tag;
-
-
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
@@ -25,8 +24,16 @@ public class Message {
         this.tag = tag;
     }
 
-    public String getAuthorName(){
+    public String getAuthorName() {
         return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setText(String text) {
@@ -52,8 +59,4 @@ public class Message {
     public void setTag(String tag) {
         this.tag = tag;
     }
-
-    public User getAuthor() { return author; }
-
-    public void setAuthor(User author) { this.author = author; }
 }
